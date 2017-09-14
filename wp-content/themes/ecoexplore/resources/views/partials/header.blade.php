c046045426e0cedd393e3529489c65f353a06650
Alisa Herr <alisa@unitymakes.us>
2017-09-14 10:57:48 -0400

    Style header

--- wp-content/themes/ecoexplore/resources/views/partials/header.blade.php
+++ wp-content/themes/ecoexplore/resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="banner">
   <nav class="">
     <div class="nav-wrapper">
-      <a class="brand-logo" href="{{ home_url('/') }}" rel="home">
+      <a class="brand-logo left" href="{{ home_url('/') }}" rel="home">
         @if (has_custom_logo())
           @php
             $custom_logo_id = get_theme_mod( 'custom_logo' );
@@ -17,19 +17,30 @@
       </a>
 
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'center hide-on-med-and-down']) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu primary hide-on-med-and-down']) !!}
 
         @if (is_user_logged_in())
-          @if (has_nav_menu('logged_in'))
-            {!! wp_nav_menu(['theme_location' => 'logged_in', 'menu_class' => 'right hide-on-med-and-down']) !!}
-          @endif
+          <ul class="menu secondary hide-on-med-and-down">
+            <li>
+              <a class="dropdown-button" href="#!" data-activates="account-dropdown">
+                @php
+                  $user = wp_get_current_user();
+                @endphp
+                {{ $user->display_name }}
+                <i class="material-icons right">arrow_drop_down</i>
+              </a>
+              @if (has_nav_menu('logged_in'))
+                {!! wp_nav_menu(['theme_location' => 'logged_in', 'menu_class' => 'dropdown-content', 'menu_id' => 'account-dropdown']) !!}
+              @endif
+            </li>
+          </ul>
         @else
           @if (has_nav_menu('logged_out'))
-            {!! wp_nav_menu(['theme_location' => 'logged_out', 'menu_class' => 'right hide-on-med-and-down']) !!}
+            {!! wp_nav_menu(['theme_location' => 'logged_out', 'menu_class' => 'menu secondary hide-on-med-and-down']) !!}
           @endif
         @endif
         <a href="#" data-activates="mobile-menu" id="mobile-menu-button" class="right button-collapse" aria-hidden="true"><i class="fa fa-bars">menu</i></a>
-        <div aria-hidden="true">
+        <div aria-hidden="true" class="hide-on-large-only">
           <ul class="side-nav" id="mobile-menu">
             {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new App\MobileNavWalker()]) !!}
 
