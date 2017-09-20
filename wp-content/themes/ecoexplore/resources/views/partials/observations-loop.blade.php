e03e1250c394c1c3ed755629be3ceff161a19c9d
Alisa Herr <alisa@unitymakes.us>
2017-09-20 08:51:09 -0400

    Add iNat feed to home page. Move observations styles to separate file.

--- wp-content/themes/ecoexplore/resources/views/partials/observations-loop.blade.php
+++ wp-content/themes/ecoexplore/resources/views/partials/observations-loop.blade.php
@@ -1,71 +1,30 @@
-<div class="observation card horizontal one">
-  <a href="#" class="mega-link" aria-hidden="true"></a>
-  <div class="card-image">
-    <img src="http://lorempixel.com/100/100/nature?100" />
-  </div>
+@php($observations = App\get_observations())
 
-  <div class="card-stacked">
-    <div class="card-content">
-      <h3><a href="#">Antlions, Lacewings &amp; Allies</a></h3>
-      <p>spotted by turkeyslippers</p>
-      <p>
-        <span class="where"><i class="material-icons">location_on</i> Asheville, NC</span>
-        <span class="when"><i class="material-icons">access_time</i> Jul 18, 2017</span>
-      </p>
-    </div>
-  </div>
-</div>
+@if (!empty($observations))
+	@foreach($observations as $o)
+		<div class="observation card horizontal">
+      <a href="#" class="mega-link" aria-hidden="true"></a>
+			<div class="card-image">
+				<img src="{{ $o->photos[0]->square_url }}" alt="" />
+			</div>
 
-<div class="observation card horizontal two">
-  <a href="#" class="mega-link" aria-hidden="true"></a>
-  <div class="card-image">
-    <img src="http://lorempixel.com/100/100/nature?200" />
-  </div>
+			<div class="card-stacked">
+				<div class="card-content">
+					<h3><a href="#">{{ $o->species_guess }}</a></h3>
+          <p>spotted by _____</p>
+					<ul>
+						<li class="where"><i class="material-icons" aria-label="Where">location_on</i> {{ $o->place_guess }}</li>
+						<li class="when"><i class="material-icons" aria-label="When">access_time</i> {{ date("M j, Y", strtotime($o->created_at)) }}</li>
+					</ul>
+				</div>
 
-  <div class="card-stacked">
-    <div class="card-content">
-      <h3><a href="#">Pipevine Swallowtail</a></h3>
-      <p>spotted by Love310</p>
-      <p>
-        <span class="where"><i class="material-icons">location_on</i> Marshall, NC</span>
-        <span class="when"><i class="material-icons">access_time</i> Jul 11, 2017</span>
-      </p>
-    </div>
-  </div>
-</div>
-
-<div class="observation card horizontal three">
-  <a href="#" class="mega-link" aria-hidden="true"></a>
-  <div class="card-image">
-    <img src="http://lorempixel.com/100/100/nature?300" />
-  </div>
-
-  <div class="card-stacked">
-    <div class="card-content">
-      <h3><a href="#">Regal Moth</a></h3>
-      <p>spotted by Jman</p>
-      <p>
-        <span class="where"><i class="material-icons">location_on</i> Asheville, NC</span>
-        <span class="when"><i class="material-icons">access_time</i> Jul 11, 2017</span>
-      </p>
-    </div>
-  </div>
-</div>
-
-<div class="observation card horizontal four">
-  <a href="#" class="mega-link" aria-hidden="true"></a>
-  <div class="card-image">
-    <img src="http://lorempixel.com/100/100/nature?100" />
-  </div>
-
-  <div class="card-stacked">
-    <div class="card-content">
-      <h3><a href="#">Grasshoppers, Crickets &amp; Katydids</a></h3>
-      <p>spotted by humancheese32</p>
-      <p>
-        <span class="where"><i class="material-icons">location_on</i> Asheville, NC</span>
-        <span class="when"><i class="material-icons">access_time</i> Jul 9, 2017</span>
-      </p>
-    </div>
-  </div>
-</div>
+				<div class="card-action">
+					<ul>
+						<li><i class="material-icons" aria-label="Points">star_border</i> Points</li>
+						<li><i class="material-icons" aira-hidden="true">cloud_upload</i> See on iNaturalist</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	@endforeach
+@endif
