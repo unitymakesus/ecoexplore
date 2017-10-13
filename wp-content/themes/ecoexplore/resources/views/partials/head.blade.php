91ebd7fd740a7fd01fa26eddafe41015a9758fb6
Alisa Herr <alisa@unitymakes.us>
2017-10-13 15:37:23 -0400

    Fix mobile menu 2 line links. Add GA tracking

--- wp-content/themes/ecoexplore/resources/views/partials/head.blade.php
+++ wp-content/themes/ecoexplore/resources/views/partials/head.blade.php
@@ -3,4 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php(wp_head())
+  @if (!is_user_logged_in())
+    @include('partials.analytics')
+  @endif
 </head>
